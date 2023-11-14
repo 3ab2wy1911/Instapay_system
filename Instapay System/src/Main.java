7d76762b0a1a9c4ba6b7f4f53572a90fa24b977e
sh3boo <ahmedshanan2021@gmail.com>
2023-11-14 06:20:16 +0200

    changed main to test my function

--- Instapay System/src/Main.java
+++ Instapay System/src/Main.java
@@ -8,10 +8,15 @@ public class Main {
         Database d= new Database();    // just for creating my dummy account list
                                         // (waiting for register function to add our list )
 
-        InstapayAccount bankAccount = new BankAccount();
-        bankAccount.showAccounts();
+//        InstapayAccount bankAccount = new BankAccount();
+//        bankAccount.showAccounts();
+//
+//        bankAccount.signIn();
 
-        bankAccount.signIn();
+
+        InstapayAccount n = new WalletAccount("username", "password", "01095454010", 1000.0);
+
+        n.payBill();
 
 
     }
